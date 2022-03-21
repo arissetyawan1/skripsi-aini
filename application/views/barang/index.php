c9013a72c5a2aaa9d7355114126e629ff8471657
Aini <aininurul.agustus@gmail.com>
2022-03-21 16:03:51 +0700

    update card

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -2,47 +2,52 @@
 		<div id="layoutSidenav_content" class="content-menu">
 			<main>
 				<div class="container-fluid px-4">
-					<h1 class="mt-4">Daftar Barang</h1>
-					<!-- <div class="container-fluid mt-3"> -->
-						<button type="button" class="btn btn-primary btn-lg">Tambah Barang</button>
-						<div class="row d-flex flex-row-reverse">
-							<div class="col-4">
-								<form action="" method="post" class="d-flex">
-									<div class="input-group">
-										<input type="search" class="form-control rounded me-2" name="keyword" id="search" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-										<button type="submit" class="btn btn-primary rounded">Search</button>
-									</div>
-								</form>
+					<h3 class="mt-4">Daftar Barang</h3>
+					<p>test</p>
+					<div class="card">
+					<div class="card-header">
+					<button type="button" class="btn btn-primary">Tambah Barang</button>
+					</div>
+					<div class="card-body">
+					<div class="row d-flex flex-row-reverse">
+					<div class="col-4">
+						<form action="" method="post" class="d-flex">
+							<div class="input-group">
+								<input type="search" class="form-control rounded me-2" name="keyword" id="search" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+								<button type="submit" class="btn btn-primary rounded">Search</button>
 							</div>
-						</div>
-					<!-- </div> -->
-					<div class="row">
-						<div class="container-fluid">
-							<table class="table table-bordered mt-3">
-								<thead>
-									<tr>
-										<td class="text-center"><strong>Nama Barang</strong></td>
-										<td class="text-center"><strong>Jenis Barang</strong></td>
-										<td class="text-center"><strong>Satuan</strong></td>
-										<td class="text-center"><strong>Harga</strong></td>
-										<td class="text-center"><strong>Jumlah Stok</strong></td>
-										<td class="text-center"><strong>Kelola</strong></td>
-									</tr>
-								</thead>
-								<tbody>
+						</form>
+					</div>
+					</div>
+					<table class="table table-bordered mt-3">
+							<thread>
+								<tr>
+									<td class="text-center"><strong>No.</td></strong>
+									<td class="text-center"><strong>Nama Barang</td></strong>
+									<td class="text-center"><strong>Jenis Barang</td></strong>
+									<td class="text-center"><strong>Satuan</td></strong>
+									<td class="text-center"><strong>Harga Jual</td></strong>
+									<td class="text-center"><strong>Jumlah Stok</td></strong>
+									<td class="text-center"><strong>Aksi</td></strong>
+								</tr>
+							</thread>
+							<tbody>
+								<tr>
 									<td>a</td>
-									<td>b</td>
-									<td>c</td>
-									<td>d</td>
-									<td>e</td>
+									<td>a</td>
+									<td>a</td>
+									<td>a</td>
+									<td>a</td>
+									<td>a</td>
 									<td class="text-center">
-										<button type="button" class="btn btn-info">Detil</button>
-										<button type="button" class="btn btn-warning">Ubah</button>
-										<button type="button" class="btn btn-danger">Hapus</button>
+									<button type="button" class="btn btn-info">Detil</button>
+									<button type="button" class="btn btn-warning">Ubah</button>
+									<button type="button" class="btn btn-danger">Hapus</button>
 									</td>
-								</tbody>
-							</table>
-						</div>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 					</div>
 				</div>
 			</main>
