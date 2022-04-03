42dc5269984108847b35826d41c1f32e9063554d
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-04-03 22:16:20 +0700

    add : jquery cdn

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,13 +5,16 @@
 	<script src="<?= base_url('assets/demo/chart-area-demo.js'); ?>"></script>
 	<script src="<?= base_url('assets/demo/chart-bar-demo.js'); ?>"></script>
 	<script src="https://cdn.jsdelivr.net/npm/simple-datatabl;s@latest" crossorigin="anonymous"></script>
-	<script src="<?= base_url("assets/vendor/jquery/jquery.min.js"); ?>"></script>
+	<!-- <script src="<?= base_url("assets/vendor/jquery/jquery.min.js"); ?>"></script> -->
 	<script src="<?= base_url(); ?>assets/vendor/jquery.easing/jquery.easing.min.js"></script>
 	<script src="<?= base_url('assets/js/datatables-simple-demo.js'); ?>"></script>
 	<script src="<?= base_url('assets/js/script.js') ?>"></script>
 	<script src="<?= base_url(); ?>assets/vendor/sweetalert2/sweetalert2.all.min.js"></script>
 	<!-- file config js -->
 	<script src="<?= base_url("assets/js/main_admin.js"); ?>"></script>
+	 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</body>
 
 	</html>
